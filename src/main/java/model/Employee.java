eaf426a59042a397e1709e9ff205950b055cdf25
LUAL6 <allard.lu@oocl.com>
2019-12-23 22:06:00 +0800

    Initial commit

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Employee {
+  private String name;
+  private Integer age;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+
+}
---
+++ src/test/java/util/EmployeeUtil.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Employee;
+
+public class EmployeeUtil {
+  public static List<Employee> createEmployees(){
+    List<Employee> employeeList = new ArrayList<>();
+    employeeList.add(createEmployee("Max",17));
+    employeeList.add(createEmployee("Sepp",18));
+    employeeList.add(createEmployee("Nina",15));
+    employeeList.add(createEmployee("Mike",51));
+    return employeeList;
+  }
+
+  public static Employee createEmployee(String name, Integer age){
+    Employee employee = new Employee();
+    employee.setName(name);
+    employee.setAge(age);
+    return employee;
+  }
+}
