fe9aec84305ddb14a7a5bc7f2aa9fcee8f163357
Amy Ouyang <amy.ouyang@oocl.com>
2019-12-24 13:34:41 +0800

    [Amy] refact

--- src/main/java/impl/EmployeeServiceImpl.java
+++ src/main/java/impl/EmployeeServiceImpl.java
@@ -3,14 +3,13 @@ package impl;
 import java.util.List;
 import java.util.stream.Collectors;
 import model.Employee;
-import util.EmployeeComparator;
 
 public class EmployeeServiceImpl {
 
   public List<Employee> findSundayEmployees() {
     EmployeeDaoImpl employeeDao = new EmployeeDaoImpl();
     List<Employee> employees = employeeDao.findAll();
-    return employees.stream().filter(employee -> employee.getAge() >= 18).sorted(new EmployeeComparator())
-        .collect(Collectors.toList());
+    return employees.stream().filter(employee -> employee.getAge() >= 18)
+            .sorted(Employee::compareTo).collect(Collectors.toList());
   }
 }
